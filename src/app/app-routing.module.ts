77c5f1b2ffa370d8d7dcc29002bc4bfb3830e780
Christian Katka <ckatka@hotmail.com>
2020-05-08 12:15:21 +0300

    fixed bug: content wouldnt go next to itsidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { TestComponent } from './test/test.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'sidenav', component: SidenavComponent },
+  { path: 'test', component: TestComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
